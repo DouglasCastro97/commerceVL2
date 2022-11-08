e851e1147cc2e87695935d55a399cdd1e66fc6f4
Douglas Castro97 <douglassccastro@gmail.com>
2022-11-08 10:07:27 -0300

    subindo modificações

--- commerce/App.js
+++ commerce/App.js
@@ -1,12 +1,27 @@
 import React from 'react'
+import {
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_700Bold
+} from '@expo-google-fonts/montserrat'
+
 import { StatusBar, SafeAreaView, View } from 'react-native'
 import CartShopping from './src/screens/cartShopping'
 
 export default function App() {
+  const [fontesApp] = useFonts({
+    MontserratRegular: Montserrat_400Regular,
+    MontserratBold: Montserrat_700Bold
+  });
+
+  if (!fontesApp) {
+    return <View />
+  }
+
   return (
     <SafeAreaView>
       <StatusBar />
       <CartShopping />
     </SafeAreaView>
-  );
-};
+  )
+}
--- commerce/src/screens/cartShopping.js
+++ commerce/src/screens/cartShopping.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Image, StyleSheet, Dimensions, Text, View } from 'react-native'
 
 import computer from '../../assets/pexels-hasan-albari-1229861.jpg'
+import logo from '../../assets/logo.png'
+import { processFontFamily } from 'expo-font'
 
 const width = Dimensions.get('screen').width
 
@@ -11,16 +13,19 @@ export default function CartShopping() {
       <Image source={computer} style={style.computer} />
       <Text style={style.title}>Detalhe da cesta</Text>
 
-      <View style={style.car}>
-        <Text style={style.name}>Carrinho de compras</Text>
-        <Text style={style.sellerName}>Iplace</Text>
-        <Text>
+      <View style={style.shoppingCart}>
+        <Text style={style.car}>Carrinho</Text>
+        <View style={style.seller}>
+          <Image source={logo} style={style.sellerImage} />
+          <Text style={style.product}>MacBook Air</Text>
+        </View>
+        <Text style={style.description}>
           O MacBook Air com chip M1 é um notebook extremamente portátil,
           versátil e rápido. Com uma linda tela Retina, tecnologia silenciosa
           sem ventoinha, design fino e bateria que dura o dia todo, você vai
           conseguir fazer tudo na velocidade da luz.
         </Text>
-        <Text>R$ 11.599</Text>
+        <Text style={style.price}>R$ 11.599</Text>
       </View>
     </>
   )
@@ -41,18 +46,40 @@ const style = StyleSheet.create({
     fontWeight: 'bold',
     padding: 16
   },
-  car: {
+  shoppingCart: {
     paddingVertical: 8,
     paddingHorizontal: 16
   },
-  name: {
+  car: {
+    color: '#000',
     fontSize: 26,
     lineHeight: 42,
-    color: '#000',
-    fontWeight: 'bold'
+    fontFamily: 'MontserratBold'
   },
-  sellerName: {
+  seller: {
+    flexDirection: 'row',
+    paddingVertical: 12
+  },
+  sellerImage: {
+    width: 32,
+    height: 32
+  },
+  product: {
+    fontSize: 16,
+    lineHeight: 26,
+    marginLeft: 12,
+    fontFamily: 'MontserratRegular'
+  },
+  description: {
+    color: '#a3a3a3',
     fontSize: 16,
     lineHeight: 26
+  },
+  price: {
+    color: '#2a9f85',
+    fontWeight: 'bold',
+    fontSize: 26,
+    lineHeight: 42,
+    marginTop: 8
   }
 })
